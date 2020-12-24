132780f2292e4355a6d5878cd5f535e9b560ca10
goodot <godo72310@gmail.com>
2020-12-24 20:19:22 +0400

    Initial commit

---
+++ EventBusTask.Console/EventBus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace EventBusTask.Console
+{
+    public class EventBus
+    {
+        private ConcurrentDictionary<string, ConcurrentQueue<Action<object>>> _dict =
+            new ConcurrentDictionary<string, ConcurrentQueue<Action<object>>>();
+
+        public void RegisterEvent(string eventName)
+        {
+            _dict.TryAdd(eventName, new ConcurrentQueue<Action<object>>());
+        }
+
+        public void Subscribe(string eventName, Action<object> action)
+        {
+            if (!_dict.ContainsKey(eventName)) return;
+            _dict[eventName].Enqueue(action);
+        }
+
+        public void Trigger(string eventName, object o)
+        {
+            ConcurrentQueue<Action<object>> actions;
+            if (!_dict.TryGetValue(eventName, out actions)) return;
+            foreach (var action in actions)
+            {
+                action.Invoke(o);
+            }
+            
+        }
+    }
+}
---
+++ EventBusTask.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace EventBusTask.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventBus = new EventBus();
+            eventBus.RegisterEvent("myEvent1");
+            eventBus.RegisterEvent("myEvent2");
+            eventBus.Subscribe("myEvent1", o => System.Console.WriteLine($"event 1: {JsonConvert.SerializeObject(o)}"));
+            eventBus.Subscribe("myEvent2", o => System.Console.WriteLine($"event 2: {JsonConvert.SerializeObject(o)}"));
+            eventBus.Subscribe("myEvent2", o => System.Console.WriteLine($"event 2: {JsonConvert.SerializeObject(o)}"));
+            eventBus.Trigger("myEvent1", new {name = "europebet 1"});
+            eventBus.Trigger("myEvent2", new {name = "europebet 2"});
+            System.Console.ReadKey();
+
+            
+        }
+
+       
+    }
+}
